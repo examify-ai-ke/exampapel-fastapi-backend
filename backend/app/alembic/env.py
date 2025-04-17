b736073a61f608fafe3b03c0cee3d515a587fe30
migoya2020 <dmigoya@gmail.com>
2025-04-17 12:52:25 +0300

    removed the rogne alembic

--- backend/app/alembic/env.py
+++ backend/app/alembic/env.py
@@ -25,7 +25,7 @@ fileConfig(config.config_file_name)
 target_metadata = SQLModel.metadata
 
 db_url = str(settings.ASYNC_DATABASE_URI)
-print(db_url)
+# print(db_url)
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
